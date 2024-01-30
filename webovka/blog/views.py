9e7f0df8ef274f5acd5bd7976b94bf9f4bbc7d99
Melich Matyáš <melic54024@mot.sps-dopravni.cz>
2024-01-30 09:24:47 +0100

    Initial commit

---
+++ webovka/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import random
+
+# Create your views here.
+
+def model_list(request):
+    random_number = random.randint(1, 100)
+    return HttpResponse(F'Random number { random_number}')
